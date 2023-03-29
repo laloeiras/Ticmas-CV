50a71583148e3137b9537249654b8d15aa010fed
laloeiras <laloeiras@gmail.com>
2023-03-28 23:16:18 -0300

    final

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const boton_email = document.querySelector("#e-mail")
 const boton_dire = document.querySelector("#dire")
 const boton_cumple = document.querySelector("#cumple")
 const boton_telefono = document.querySelector("#telefono")
+const boton_habilidades = document.querySelector("#habilidades")
+const boton_experiencia = document.querySelector("#experiencia")
+const boton_estudios = document.querySelector("#estudios")
 console.log(datos)
 /* datos.innerHTML = "cualquier cosa" */
 boton_nombre.addEventListener("click",function(){
@@ -22,4 +25,13 @@ boton_cumple.addEventListener("click",function(){
 })
 boton_telefono.addEventListener("click",function(){
     datos.innerHTML = "(865) 259-9275"
+})
+boton_habilidades.addEventListener("click",function(){
+    datos.innerHTML = "manejo de office"
+})
+boton_experiencia.addEventListener("click",function(){
+    datos.innerHTML = "ayudante de albañil<br> ayudante de cocina"
+})
+boton_estudios.addEventListener("click",function(){
+    datos.innerHTML = "primario completo EEB Sarmiento <br> secundario completo EES Sarmiento"
 })
